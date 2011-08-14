866036b1398914b14e62428eb58db09dfd219c54
Jason Brennan <i.jasonbrennan@gmail.com>
2011-08-13 22:15:27 -0400

    Closer to Bindings working. Added the Article class from older project.

---
+++ Editor2/JBArticle.h
@@ -0,0 +1,59 @@
+//
+//  JBArticle.h
+//  Editor
+//
+//  Created by Jason Brennan on 10-04-28.
+//  Copyright 2010 Jason Brennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#define kHeadlineKey @"headline"
+#define kLedeKey @"lede"
+#define kAuthorNameKey @"author"
+#define kByLineKey @"by_line"
+#define kAltTextKey @"alt_text"
+#define kBodyFileKey @"body_text" // not a bug!!
+#define kSourceLinkKey @"source"
+#define kCreatedAtKey @"created_at"
+#define kUpdatedAtKey @"updated_at"
+
+@interface JBArticle : NSObject {
+	NSString *_headline;
+	NSString *_lede;
+	NSString *_byLine;
+	NSString *_altText;
+	NSString *_authorName;
+	NSString *_authorEmail;
+	NSString *_sourceLink;
+	NSString *_bodyFile;
+	NSString *_metaFile;
+	NSDate *_createdAtDate;
+	NSDate *_updatedAtDate;
+	
+	NSString *_bodyText;
+	
+	BOOL _articleUpdated;
+	
+}
+
+@property (nonatomic, copy) NSString *headline;
+@property (nonatomic, copy) NSString *lede;
+@property (nonatomic, copy) NSString *byLine;
+@property (nonatomic, copy) NSString *altText;
+@property (nonatomic, copy) NSString *authorName;
+@property (nonatomic, copy) NSString *authorEmail;
+@property (nonatomic, copy) NSString *sourceLink;
+@property (nonatomic, copy) NSString *bodyFile;
+@property (nonatomic, copy) NSString *metaFile;
+@property (nonatomic, retain) NSDate *createdAtDate;
+@property (nonatomic, retain) NSDate *updatedAtDate;
+@property (nonatomic, copy) NSString *bodyText;
+@property (nonatomic, getter=isArticleUpdated) BOOL articleUpdated;
+
+
+- (id)initNewArticle;
+- (void)saveIfNeeded;
+- (void)forceSave;
+
+@end
---
+++ Editor2/JBArticle.m
@@ -0,0 +1,173 @@
+//
+//  JBArticle.m
+//  Editor
+//
+//  Created by Jason Brennan on 10-04-28.
+//  Copyright 2010 Jason Brennan. All rights reserved.
+//
+
+#import "JBArticle.h"
+
+
+@implementation JBArticle
+@synthesize headline = _headline;
+@synthesize lede = _lede;
+@synthesize byLine = _byLine;
+@synthesize altText = _altText;
+@synthesize authorName = _authorName;
+@synthesize authorEmail = _authorEmail;
+@synthesize sourceLink = _sourceLink;
+@synthesize bodyFile = _bodyFile;
+@synthesize metaFile = _metaFile;
+@synthesize createdAtDate = _createdAtDate;
+@synthesize updatedAtDate = _updatedAtDate;
+@synthesize bodyText = _bodyText;
+@synthesize articleUpdated = _articleUpdated;
+
+
+#pragma mark -
+#pragma mark Object Lifecycle
+
+- (id)init {
+	if (self = [super init]) {
+		_articleUpdated = NO;
+	}
+	
+	return self;
+}
+
+
+- (id)initNewArticle {
+	if (self = [super init]) {
+		_articleUpdated = YES;
+		self.createdAtDate = [NSDate date];
+		self.updatedAtDate = [NSDate date];
+	}
+	
+	return self;
+}
+
+
+
+
+- (void)forceSave {
+	_articleUpdated = YES;
+	[self saveIfNeeded];
+}
+
+
+- (void)saveIfNeeded {
+	
+	// Don't need to save. Fail
+	if (!_articleUpdated)
+		return;
+	
+	// Create a filename and filePath for the markdown/body text
+	
+	NSString *bodyFileName = self.bodyFile;
+	
+	if (nil == bodyFileName) {
+		bodyFileName = [[[self.headline lowercaseString] stringByReplacingOccurrencesOfString:@" " withString:@"_"] stringByAppendingString:@".md"];
+	}
+	
+	NSString *directoryPath = [[[NSUserDefaults standardUserDefaults] stringForKey:@"articleDirectory"] stringByAppendingString:@"/"];
+	NSString *bodyFilePath = [directoryPath stringByAppendingString:bodyFileName];
+	
+	// Save the body text out to the markdown file
+	NSError *bodyFileError = nil;
+	if (![self.bodyText writeToFile:bodyFilePath atomically:YES encoding:NSUTF8StringEncoding error:&bodyFileError]) {
+		NSLog(@"There was an error writing the article's body to the markdown file. Headline: %@, Error: %@", self.headline, [bodyFileError localizedDescription]);
+		return;
+	}
+	
+	
+	// The JS filename and path
+	NSString *jsFileName = self.metaFile;
+	if (nil == jsFileName) {
+		jsFileName = [[[self.headline lowercaseString] stringByReplacingOccurrencesOfString:@" " withString:@"_"] stringByAppendingString:@".js"];
+	}
+	
+	NSString *jsFilePath = [directoryPath stringByAppendingString:jsFileName];
+	
+	// Create the Dictionary
+	NSMutableDictionary *articleDictionary = [NSMutableDictionary dictionaryWithCapacity:10];
+	[articleDictionary setObject:self.headline ? self.headline : @"" forKey:kHeadlineKey];
+	[articleDictionary setObject:self.lede ? self.lede : @"" forKey:kLedeKey];
+	[articleDictionary setObject:self.byLine ? self.byLine : @"" forKey:kByLineKey];
+	[articleDictionary setObject:self.altText ? self.altText : @"" forKey:kAltTextKey];
+	[articleDictionary setObject:self.authorName ? self.authorName : @"" forKey:kAuthorNameKey];
+	//[articleDictionary setObject:self.authorEmail forKey:kAuthorEmailKey];
+	[articleDictionary setObject:self.sourceLink ? self.sourceLink : @"self" forKey:kSourceLinkKey];
+	
+	
+	// Format the dates so they may be saved as a string
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	[dateFormatter setDateStyle:NSDateFormatterLongStyle];
+	[dateFormatter setTimeStyle:NSDateFormatterFullStyle];
+	
+	NSString *createdAtString = [[dateFormatter stringFromDate:self.createdAtDate] stringByReplacingOccurrencesOfString:@"," withString:@""];
+	NSString *updatedAtString = [[dateFormatter stringFromDate:self.updatedAtDate] stringByReplacingOccurrencesOfString:@"," withString:@""];
+	
+	
+	
+	NSDate *testDate = [NSDate dateWithNaturalLanguageString:createdAtString];
+	
+	if ([testDate isEqualToDate:self.createdAtDate])
+		NSLog(@"Round trip dates!!");
+	
+	//return;
+	
+	[articleDictionary setObject:createdAtString ? createdAtString : @"" forKey:kCreatedAtKey];
+	[articleDictionary setObject:updatedAtString ? updatedAtString : @"" forKey:kUpdatedAtKey];
+	[articleDictionary setObject:bodyFileName ? bodyFileName : @"" forKey:kBodyFileKey];
+	
+	
+	// Create a JSON representation for the article
+//	SBJSON *articleJSON = [[SBJSON alloc] init];
+//	[articleJSON setHumanReadable:YES];
+//	NSString *jsonString = [articleJSON stringWithObject:articleDictionary];
+	
+	
+	NSError *error = nil;
+	NSData *jsonData = [NSJSONSerialization dataWithJSONObject:articleDictionary options:NSJSONWritingPrettyPrinted error:&error];
+	if (nil == jsonData) {
+		NSLog(@"There was an error transforming the articleDictionary to JSON data. Error: %@", [error localizedDescription]);
+	}
+	
+	
+	NSError *jsonFileError = nil;
+	if (![jsonData writeToFile:jsFilePath options:NSDataWritingAtomic error:&jsonFileError]) {
+		NSLog(@"There was an error writing the JS file to disk. Headline: %@, error: %@", self.headline, [jsonFileError localizedDescription]);
+		return;
+	}
+	
+	
+	_articleUpdated = NO; // because we've saved, now the article can again be changed so we must track that
+}
+
+
+#pragma mark -
+#pragma mark Custom accessors
+
+
+- (void)setArticleUpdated:(BOOL)isUpdated {
+	_articleUpdated = isUpdated;
+	
+	if (isUpdated) {
+		[self setUpdatedAtDate:[NSDate date]];
+	}
+}
+
+//- (void)setHeadline:(NSString *)newHeadline {
+//	if ([_headline isEqualToString:newHeadline])
+//		return;
+//	
+//	if (nil != _headline || nil != newHeadline)
+//		_articleUpdated = YES;
+//	
+//		
+//}
+
+
+
+@end
